8b2b9e5ede9be9174e0b381c083668b3b54caa7b
Adan Rodriguez <adanrodriguez@Kourtneys-MBP.hitronhub.home>
2020-05-19 02:49:19 -0500

    got the form schema all written out. Added error messages. All there is left to do is to get the post request and get his puppy to MVP

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,10 +2,10 @@ import React, {useState} from 'react';
 import * as yup from 'yup';
 
 const formSchema = yup.object().shape({
-    name: yup.string().required(),
-    email: yup.string().email().required(),
-    password: yup.string().password().required(),
-    terms: yup.boolean().oneOf([true])
+    name: yup.string().required("dude, put your name in"),
+    email: yup.string().email().required("That's not a real email, shitbag"),
+    password: yup.string(),
+    terms: yup.boolean().oneOf([true], "Oh, so you disagree? Get the hell out of here")
 })
 
 const Form = () => {
@@ -16,16 +16,46 @@ const Form = () => {
         terms: false
     })
 
+    //error handling state
+    const [errors, SetErrors] = useState({
+        name: "",
+        email: "",
+        password: "",
+        terms: ""
+    })
+
+    const validate = (e) => {
+        yup.reach(formSchema, e.target.name)
+        .validate(e.target.value)
+        .then(valid => {
+            SetErrors({
+                ...errors,
+                [e.target.name]: ""
+            })
+        })
+        .catch(err => {
+            // console.log(err.errors)
+            SetErrors({
+                ...errors,
+                [e.target.name]:err.errors[0]
+            })
+        })
+    }
+
     //onSubmit function
     const formSubmit = (e) => {
         e.preventDefault();
 
-        setFormState({name: "", email: "", password: "", terms: false})
+      
     }
 
     //onChange function
 
     const inputChange = e => {
+        e.persist()
+
+        validate(e)
+
         let value = e.target.type === "checkbox" ? e.target.checked : e.target.value
         setFormState({...formState, [e.target.name]: value})
     }
@@ -36,10 +66,13 @@ const Form = () => {
             <form onSubmit = {formSubmit}>
                 <input type="text" name="name"  value = {formState.name} onChange = {inputChange} placeholder = "Name:"></input>
                 <input type ="text" name="email"  value = {formState.email} onChange = {inputChange} placeholder = "Email:"></input>
+                 {errors.email.length > 0 ? <p>{errors.email}</p> : null}
                 <input type = "text" name="password" value = {formState.password} onChange = {inputChange} placeholder = "Password:"></input>
+                 {errors.password.length > 0 ? <p>{errors.password}</p>: null}
                 <label htmlFor = "terms">
                   I have read the Terms and Conditions:
-                  <input type = "checkbox" id = "terms" name = "terms" value = {formState} onChange = {inputChange}/>
+                  <input type = "checkbox" id = "terms" name = "terms" value = {formState.terms} onChange = {inputChange}/>
+                 {errors.terms.length > 0 ? <p>{errors.terms}</p>: null }
                   <button type="button">Submit</button>
                 </label>
             </form>
